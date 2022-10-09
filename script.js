f671642471808fbd74bcc621aea8c0f2cf823dc3
Andree Ismodes <andreeismodes@gmail.com>
2022-10-09 03:48:44 -0500

    Proyecto simple terminado, habrán actualizaciones

--- script.js
+++ script.js
@@ -1,42 +1,22 @@
-// const defaultColor = "#000000";
 
-// window.addEventListener("load", startup);
-
-
-// function startup() {
-//     const colorPicker = document.querySelector('#colorPicker');
-    // const body = document.querySelector('#body');
-    
-//     body.style.backgroundColor = "#98b37d";
-//     colorPicker.value = defaultColor;
-//     colorPicker.addEventListener("input", updateFirst);
-//     colorPicker.addEventListener('click', (e) => {
-//         console.log(e)
-//     })
-// }
-
-// function updateFirst(event) {
-//     const salida = document.querySelector('#salida')
-
-//     console.log(event)
-//     if (body) {
-//         body.style.backgroundColor = event.target.value;
-
-//         salida.innerHTML = event.target.value
-//     }
-// }
-
-const body = document.querySelector('#body');
-    body.style.backgroundColor = "#e5c5c5";
-
+const colorDefault = '#40D645';
+const salidaTexto = document.querySelector('#salida')
+const body = document.querySelector('#body').style;
 const colorPicker = new iro.ColorPicker('#colorPicker', {
     // Set the size of the color picker
-    width: 320,
-
+    width: 250,
+    
     // Set the initial color to pink
-    color: "#e5c5c5"
+    color: colorDefault
 });
 
+
+body.backgroundColor = colorDefault;
+salidaTexto.innerHTML = colorDefault;
+
+
 colorPicker.on('color:change', function(color) {
-    body.style.backgroundColor = color.hexString;
+    
+    body.backgroundColor = color.hexString;
+    salidaTexto.innerHTML = color.hexString;
 })
